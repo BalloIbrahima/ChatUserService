a77826544231d67958f107cd53156d61c7f2b9db
BalloIbrahima <ibrahimaballo01@gmail.com>
2023-04-04 12:06:37 +0000

    add controller

---
+++ src/main/java/com/talkhours/userservice/Controllers/UtilisateurController.java
@@ -0,0 +1,12 @@
+package com.talkhours.userservice.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/utilisateur")
+//@CrossOrigin(origins = {"http://localhost:4200","http://localhost:8200","http://localhost:8100"}, maxAge = 3600, allowCredentials="true")
+//@Api(value = "utilisateur", description = "Le controller pour l'entité utilisateur.")
+public class UtilisateurController {
+    
+}
