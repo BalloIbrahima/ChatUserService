9c865086bc6eb51782a479476349918a49c2d763
BalloIbrahima <ibrahimaballo01@gmail.com>
2023-04-04 12:05:52 +0000

    add services impl

---
+++ src/main/java/com/talkhours/userservice/Implementations/UtilisateurImpl.java
@@ -0,0 +1,9 @@
+package com.talkhours.userservice.Implementations;
+import org.springframework.stereotype.Service;
+
+import com.talkhours.userservice.Services.UtilisateurService;
+
+@Service
+public class UtilisateurImpl implements UtilisateurService {
+    
+}
