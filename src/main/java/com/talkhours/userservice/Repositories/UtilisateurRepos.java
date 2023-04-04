30a9581dc03045087628d7d7c428495ff4362942
BalloIbrahima <ibrahimaballo01@gmail.com>
2023-04-04 12:05:00 +0000

    addrepos

--- src/main/java/com/talkhours/userservice/Repositories/UtilisateurRepos.java
+++ src/main/java/com/talkhours/userservice/Repositories/UtilisateurRepos.java
@@ -1,9 +1,10 @@
-package com.bugbooks.userservice.Repositories;
+package com.talkhours.userservice.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.bugbooks.userservice.Entites.Utilisateur;
+import com.talkhours.userservice.Entities.Utilisateur;
+
 @RepositoryRestResource
 public interface UtilisateurRepos extends JpaRepository<Utilisateur,Long>{
     Utilisateur findByEmail(String email);
